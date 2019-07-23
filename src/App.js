665c6abc3a47cd1cfc281abdf33e74c0b14745ab
=DamjanRadev <radevdamjan@gmail.com>
2019-07-23 12:39:08 +0200

    Added state, props, and method handler to manipulate the dom onClick

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
-
+import React, {Component} from 'react';
+import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hi, I'm a React App</h1>
-  //   </div>
-  // );
-  return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Hi, I\'m a React App!!!'));
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age:29},
+      {name: 'Stephanie', age:26}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({persons: [
+      {name: 'Damjan', age: 28},
+      {name: 'Manu', age:29},
+      {name: 'Stephanie', age:21}
+    ]});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I'm a React app</h1>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: basketball</Person> 
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+      );
+    }
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Hi, I\'m a React App!!!'));
 }
 
 export default App;
